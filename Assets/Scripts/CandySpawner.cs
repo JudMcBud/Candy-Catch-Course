ecdac264439c0c3cbd304c67540c0fa481c2f24e
Judson Hester <judsonhester01@gmail.com>
2022-03-01 12:25:30 -0600

    Initial commit

---
+++ Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+	
+	[SerializeField]
+	float maxX;
+	[SerializeField]
+	float spawnInterval;
+	[SerializeField]
+	GameObject[] candies;
+	
+	public static CandySpawner instance;
+	
+	void Awake()
+	{
+		if(instance ==  null)
+		{
+			instance = this;
+		}
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+		StartSpawningCandies();
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+    	
+    }
+    
+	void SpawnCandy()
+	{
+		int rand = Random.Range(0, candies.Length);
+		
+		float randPos = Random.Range(-maxX, maxX);
+		
+		Vector3 spawnBounds = new Vector3(randPos, transform.position.y, transform.position.z);
+		
+		Instantiate(candies[rand], spawnBounds, transform.rotation);
+	}
+	
+	IEnumerator SpawnCandies()
+	{
+		yield return new WaitForSeconds(1.5f);
+		
+		while(true)
+		{
+			SpawnCandy();
+			
+			yield return new WaitForSeconds(spawnInterval);
+		}
+	}
+	
+	public void StartSpawningCandies()
+	{
+		StartCoroutine("SpawnCandies");
+	}
+	
+	public void StopSpawningCandies()
+	{
+		StopCoroutine("SpawnCandies");
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	int score = 0;
+	int lives = 3;
+	
+	bool gameOver = false;
+	
+	public GameObject livesPanel;
+	public GameObject scorePanel;
+	public GameObject gameOverPanel;
+	
+	public static GameManager instance;
+	
+	public Text scoreText;
+	public Text gameOverScore;
+	
+	private void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	public void IncrementScore()
+	{
+		if(!gameOver)
+		{
+		score++;
+		scoreText.text = score.ToString();
+			//print(score);
+		}
+	}
+	
+	public void DecrementLives()
+	{
+		if(lives > 0)
+		{
+			lives--;
+			
+			livesPanel.transform.GetChild(lives).gameObject.SetActive(false);
+		}
+		
+		if(lives <= 0)
+		{
+			gameOver = true;
+			
+			GameOver();
+		}
+	}
+	
+	public void GameOver()
+	{
+		
+		CandySpawner.instance.StopSpawningCandies();
+		GameObject.Find("Player").GetComponent<PlayerController>().canMove = false;
+		livesPanel.SetActive(false);
+		scorePanel.SetActive(false);
+		gameOverPanel.SetActive(true);
+		gameOverScore.text = score.ToString();
+		print("GameOver");
+	}
+	
+	public void Restart()
+	{
+		SceneManager.LoadScene("Game");
+	}
+	
+	public void BackToMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
